d3d1c46b9f12dafdb643186a6189d8ff0ed43395
fbmcarmo <fbrunomcarmo@gmail.com>
2026-03-07 18:45:14 -0300

    feature: validate user

--- src/modules/users/users.routes.js
+++ src/modules/users/users.routes.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const Controller = require("./users.controller");
 
+const { rotaProtegida } = require("../../shared/middlewares/token.middleware");
+const { validarUsuario, apenasAdmin } = require("../../shared/middlewares/user.middleware");
+
 const usersController = new Controller();
 
 router.post("/", (req, res, next) => usersController.create(req, res, next));
@@ -9,15 +12,20 @@ router.get("/confirm", (req, res, next) =>
   usersController.confirmEmail(req, res, next),
 );
 
-router.get("/", (req, res, next) => usersController.getAll(req, res, next));
-router.get("/:id", (req, res, next) => usersController.getById(req, res, next));
-
-router.patch("/:id", (req, res, next) =>
-  usersController.update(req, res, next),
+router.get("/", rotaProtegida, validarUsuario, (req, res, next) =>
+  usersController.getAll(req, res, next)
 );
 
-router.delete("/:id", (req, res, next) =>
-  usersController.delete(req, res, next),
+router.get("/:id", rotaProtegida, validarUsuario, (req, res, next) =>
+  usersController.getById(req, res, next)
+);
+
+router.patch("/:id", rotaProtegida, validarUsuario, apenasAdmin, (req, res, next) =>
+  usersController.update(req, res, next)
+);
+
+router.delete("/:id", rotaProtegida, validarUsuario, apenasAdmin, (req, res, next) =>
+  usersController.delete(req, res, next)
 );
 
 module.exports = router;
